3e53d0803b4143cd851c7d6d53e8e1ebf0ef375e
Oscar Cuatrecasas <oscar@batou.xyz>
2023-07-09 12:46:58 +0200

    Changed serializers

--- src/index/pq_idx.rs
+++ src/index/pq_idx.rs
@@ -244,7 +244,7 @@ impl<E: node::FloatElement + DeserializeOwned, T: node::IdxType + DeserializeOwn
     ann_index::SerializableIndex<E, T> for PQIndex<E, T>
 {
     fn load(data: Vec<u8>) -> Result<Self, &'static str> {
-        let mut instance: PQIndex<E, T> = bincode::deserialize_from(Cursor::new(String::from_utf8(data).unwrap().into_bytes())).unwrap();
+        let mut instance: PQIndex<E, T> = bincode::deserialize_from(Cursor::new(String::from_utf8(data)?.into_bytes()))?;
         instance._nodes = instance
             ._nodes_tmp
             .iter()
@@ -485,7 +485,7 @@ impl<E: node::FloatElement + DeserializeOwned, T: node::IdxType + DeserializeOwn
     ann_index::SerializableIndex<E, T> for IVFPQIndex<E, T>
 {
     fn load(data: Vec<u8>) -> Result<Self, &'static str> {
-        let mut instance: IVFPQIndex<E, T> = bincode::deserialize_from(Cursor::new(String::from_utf8(data).unwrap().into_bytes())).unwrap();
+        let mut instance: IVFPQIndex<E, T> = bincode::deserialize_from(Cursor::new(String::from_utf8(data)?.into_bytes())).unwrap();
         instance._nodes = instance
             ._nodes_tmp
             .iter()
