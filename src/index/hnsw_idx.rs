56cd7ddcfa8b695d73f89e3dc3f059a02decef22
Oscar Cuatrecasas <oscar@batou.xyz>
2023-07-09 06:13:18 +0200

    Changed serializers

--- src/index/hnsw_idx.rs
+++ src/index/hnsw_idx.rs
@@ -645,7 +645,7 @@ impl<E: node::FloatElement + DeserializeOwned, T: node::IdxType + DeserializeOwn
     ann_index::SerializableIndex<E, T> for HNSWIndex<E, T>
 {
     fn load(data: Vec<u8>) -> Result<Self, &'static str> {
-        let mut instance: HNSWIndex<E, T> = bincode::deserialize_from(String::from_utf8(data)).unwrap();
+        let mut instance: HNSWIndex<E, T> = bincode::deserialize_from(String::from_utf8(data)?).unwrap();
         instance._nodes = instance
             ._nodes_tmp
             .iter()
